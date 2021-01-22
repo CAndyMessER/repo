17239505e58c756b085b0ba70cfed954bc353a78
Andrea <acommesso73@gmail.com>
2021-01-22 21:56:55 +0100

    file base+ prove

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fetch =require("node-fetch")
 	 console.log("ciao")
 	 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(data=> data.json())
-	.then(data=> {
-		let acc= data.reduce((acc,e)=>{
-		return acc+=e.userId
-		},0)
-
-		console.log(acc)})
-
-/*	.filter((e)=>{if(e.userId===10){return true}})
-.forEach((e)=> 
-console.log(e.id+ " " +e.body)) */
+	.then(data=>{
+		for (let i=0 ; i<=50 ;i++){
+			data[i].userId+=1
+			if (data[i].userId%2===0) {
+				console.log( data[i])
+				
+			}
+				
+			}
+		}) 
---
+++ index2.js
@@ -0,0 +1,18 @@
+"use strict"
+const fetch =require("node-fetch")	
+console.log("ciao")
+fetch('https://jsonplaceholder.typicode.com/posts')
+	.then(data => data.json())
+	.then(data => {
+		let acc = data.reduce((acc, e) => {
+			return acc+=e.userId
+		}, 0)
+		console.log(acc)	
+		data
+			.filter((e) => {if(e.userId===10){return true}})
+			.forEach((e) => console.log(e.id+ " " +e.body)) 
+	
+		let some = data.map(e => {
+			return e.userId += 1
+		})
+	console.log(some)})
